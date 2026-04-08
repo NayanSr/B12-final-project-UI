01867b6b4bd31dc0e2a8465992a5c2e84521f2bf
Nayan Suter <nayankbps@gmail.com>
2026-04-08 22:07:04 +0600

    use react hook form💖

--- src/pages/Auth/Registration.jsx
+++ src/pages/Auth/Registration.jsx
@@ -1,23 +1,59 @@
-// import { useForm } from "react-hook-form"
+import { useForm } from "react-hook-form";
 
 const Registration = () => {
-// const {register}= useForm()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const handleRegistration = (data) => {
+    console.log(data);
+  };
 
   return (
     <div>
-        <form >
-           <fieldset className="fieldset">
+      <form onSubmit={handleSubmit(handleRegistration)}>
+        <fieldset className="fieldset">
+          {/* Email */}
           <label className="label">Email</label>
-          <input type="email" className="input" placeholder="Email" />
-          <label className="label">Password</label>
-          <input type="password" className="input" placeholder="Password" />
-          <div><a className="link link-hover">Forgot password?</a></div>
-          <button className="btn btn-neutral mt-4">Register</button>
-         
-        </fieldset>
-        </form>
-    </div>
-  )
-}
+          <input
+            type="email"
+            {...register("email", { required: true })}
+            className="input"
+            placeholder="Email"
+          />
+          {errors.email?.type === "required" && (
+            <p className="text-red-700 text-lg">Email could not be empty.</p>
+          )}
 
-export default Registration
+          {/* Password */}
+          <label className="label">Password</label>
+          <input
+            type="password"
+            {...register("password", { required: true, minLength: 6,pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]).+$/ })}
+            className="input"
+            placeholder="Password"
+            
+          />
+          {errors.password?.type === "required" && (
+            <p className="text-red-700 text-lg">Password could not be empty.</p>
+          )}
+          {errors.password?.type === "minLength" && (
+            <p className="text-red-700 text-lg">
+              Password mut be atlest 6 character
+            </p>
+          )}
+          {errors.password?.type==='pattern' && <p className="text-lg text-red-500">Password must contain one uppercase one lowercase one number and one special character.</p> }
+          <div>
+            <a className="link link-hover">Forgot password?</a>
+          </div>
+
+          <button className="btn btn-neutral mt-4">Register</button>
+        </fieldset>
+      </form>
+    </div>
+  );
+};
+
+export default Registration;
